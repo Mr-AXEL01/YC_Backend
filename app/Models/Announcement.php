08d1710d71a498a9b4a315a3d6e1c9b6b6d127f2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-26 22:21:03 +0000

    craete the fillable for the announcements

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Announcement extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'title',
+        'type',
+        'date',
+        'description',
+        'location',
+        'required_skills',
+        'organizer_id',
+
+    ];
 
     public function user(): BelongsTo
     {
