442f23d0bff394a55124471980aa5788fa08e5a1
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-26 23:04:30 +0000

    set the hasMany relation in announcement for application

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Announcement extends Model
 {
@@ -25,4 +26,10 @@ class Announcement extends Model
     {
         return $this->belongsTo(User::class ,'organizer_id');
     }
+
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
+    }
+
 }
