4b6a4a4e3eb1b23a91bb1e3923868f876a6c1b4d
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-26 23:33:31 +0000

    set the rules for the announcements

--- app/Http/Requests/AnnouncementRequest.php
+++ app/Http/Requests/AnnouncementRequest.php
@@ -11,7 +11,7 @@ class AnnouncementRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class AnnouncementRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'type' => 'required|string|max:255',
+            'date' => 'required|date',
+            'description' => 'required|string',
+            'location' => 'required|string|max:255',
+            'required_skills' => 'required|array',
         ];
     }
 }
