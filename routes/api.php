6d346823c99d8e7980c778e256f24e06e4347c60
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-27 11:15:13 +0000

    create route for storing the announcement

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,8 +17,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
+//---------Authentification-------//
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 Route::post('logout', [AuthController::class, 'logout']);
 Route::post('refresh', [AuthController::class, 'refresh']);
+
+//--------- organizer ----------//
+Route::post('create_Announcement', [AnnouncementController::class, 'store']);
