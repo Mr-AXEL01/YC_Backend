a8cf9ffa43f5ac4fb02c9768a8bca76056efafc8
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-27 10:57:49 +0000

    create amethod to get all the the Announcements that exists

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Announcement;
 use Illuminate\Http\Request;
 
 class AnnouncementController extends Controller
 {
-    //
+    /*
+    * create a method to list the Announcements the exist.
+    */
+    public function index()
+    {
+        $announcements = Announcement::all();
+        return response()->json($announcements);
+    }
 }
