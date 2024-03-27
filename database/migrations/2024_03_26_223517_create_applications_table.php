a97d33a0233443e578840356c8493f9f86c6cbac
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-27 22:36:35 +0000

    set a status for the application

--- database/migrations/2024_03_26_223517_create_applications_table.php
+++ database/migrations/2024_03_26_223517_create_applications_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('volunteer_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId('announcement_id')->constrained('announcements')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->enum('status', ['pending','approved', 'refused']);
             $table->timestamps();
         });
     }
