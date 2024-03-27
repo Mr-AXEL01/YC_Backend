0787ed7e8b692ff81c1b22c9f8ae80097e6bd90c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-27 16:12:29 +0000

    create method to store application of volunteer

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApplicationRequest;
+use App\Models\Announcement;
+use App\Models\Application;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-    //
+    public function store(ApplicationRequest $request, Announcement $announcement)
+    {
+        $validatedData = $request->validated();
+
+        $application = new Application();
+        $application->announcement_id = $announcement->id;
+        $application->volunteer_id = $validatedData['volunteer_id'];
+        $application->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Application submitted successfully.',
+            'application' => $application,
+        ]);
+    }
 }
