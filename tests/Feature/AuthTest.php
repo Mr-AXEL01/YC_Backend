fa8a2daa15799e8e98a9808931584429eea87245
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-28 12:22:24 +0000

    create feature to test the login

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -9,12 +10,25 @@ use Tests\TestCase;
 class AuthTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Feature test for login.
      */
-    public function test_example(): void
+    public function testLogin()
     {
-        $response = $this->get('/');
+        $user = factory(User::class)->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
 
-        $response->assertStatus(200);
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token',
+                'token_type',
+                'expires_in',
+            ]);
     }
 }
