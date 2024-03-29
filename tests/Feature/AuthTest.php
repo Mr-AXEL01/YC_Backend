4862ff3106ad016d690cc0462b113f1c0aaecca3
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-29 12:19:40 +0000

    create test for the registrement

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -3,29 +3,42 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    /**
+     * Feature test for user registration.
+     */
+    public function testRegister()
+    {
+
+
+        $userData = [
+            'name' => 'Test User',
+            'email' => 'test3@example.com',
+            'password' => 'password',
+            'role' => 'volunteer',
+        ];
+
+        $response = $this->json("POST", '/api/register', $userData);
+
+        $response->assertStatus(200);
+
+    }
+
+
     /**
      * Feature test for login.
      */
     public function testLogin()
     {
         $user = User::factory()->create();
-
         $response = $this->postJson('/api/login', [
-            'email' => 'test@example.com',
+            'email' => $user->email,
             'password' => 'password',
         ]);
 
-        $response->assertStatus(200)
-            ->assertJsonStructure([
-                'access_token',
-                'token_type',
-                'expires_in',
-            ]);
+        $response->assertStatus(200);
     }
 }
